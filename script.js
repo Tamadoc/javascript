d7b6480f5cff168dfc57a5c972c5705a4d06bf57
Tamadoc <teri.sandstedt@gmail.com>
2021-05-12 12:08:03 +0200

    Add X's and O's

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function set(id) {
-  id.innerHTML = "O";
+let cross = true;
+
+function set(square) {
+  if (square.innerHTML === "") {
+    var turn;
+    if (cross) {
+      turn = "X";
+    } else {
+      turn = "O";
+    }
+    square.innerHTML = turn;
+    cross = !cross;
+  }  
 }
