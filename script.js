4f261f8735857da389bc0b847104e30dfe4c50ec
Tamadoc <teri.sandstedt@gmail.com>
2021-05-14 17:55:41 +0200

    Refactored to make use of javascript 'truthiness'

--- script.js
+++ script.js
@@ -1,64 +1,56 @@
-let cross = true;
+let isCross = true;
 let gameOver = false;
 
 function set(square) {
-  if (square.innerHTML === "" && !gameOver) {
-    var turn;
-    if (cross) {
-      turn = "X";
-    } else {
-      turn = "O";
-    }
+  if (!square.innerHTML && !gameOver) {
+    let turn = isCross ? "X" : "O";
     square.innerHTML = turn;
-    cross = !cross;
-  } 
-
-  checkWinState();
+    
+    checkWinState();
+    isCross = !isCross;
+  }
 }
 
 function checkWinState() {
-  let topLeft = document.getElementById('tl').innerHTML;
-  let topMiddle = document.getElementById('tm').innerHTML;
-  let topRight = document.getElementById('tr').innerHTML;
+  const topLeft = document.getElementById('tl').innerHTML;
+  const topMiddle = document.getElementById('tm').innerHTML;
+  const topRight = document.getElementById('tr').innerHTML;
 
-  let middleLeft = document.getElementById('ml').innerHTML;
-  let middleMiddle = document.getElementById('mm').innerHTML;
-  let middleRight = document.getElementById('mr').innerHTML;
+  const middleLeft = document.getElementById('ml').innerHTML;
+  const middleMiddle = document.getElementById('mm').innerHTML;
+  const middleRight = document.getElementById('mr').innerHTML;
 
-  let bottomLeft = document.getElementById('bl').innerHTML;
-  let bottomMiddle = document.getElementById('bm').innerHTML;
-  let bottomRight = document.getElementById('br').innerHTML;
-  let winner = "";
+  const bottomLeft = document.getElementById('bl').innerHTML;
+  const bottomMiddle = document.getElementById('bm').innerHTML;
+  const bottomRight = document.getElementById('br').innerHTML;
+  
+  let winner = null;
 
-  if ( topLeft != "" && topMiddle != "" && topRight != "" 
-    && middleLeft != "" && middleMiddle != "" && middleRight != "" 
-    && bottomLeft  != "" && bottomMiddle  != "" && bottomRight != ""
+  if ( topLeft && topMiddle && topRight 
+    && middleLeft && middleMiddle && middleRight  
+    && bottomLeft && bottomMiddle && bottomRight
     ) {
     winner = "no one"
   }
   
-  if ( topLeft == topMiddle && topMiddle == topRight && topRight != ""
-    || middleLeft == middleMiddle && middleMiddle == middleRight && middleRight != ""
-    || bottomLeft == bottomMiddle && bottomMiddle == bottomRight && bottomRight != ""
-    || topLeft == middleLeft && middleLeft == bottomLeft && bottomLeft != ""
-    || topMiddle == middleMiddle && middleMiddle == bottomMiddle && bottomMiddle != ""
-    || topRight == middleRight && middleRight == bottomRight && bottomRight != ""
-    || topLeft == middleMiddle && middleMiddle == bottomRight && bottomRight != ""
-    || topRight == middleMiddle && middleMiddle == bottomLeft && bottomLeft != ""
+  if ( topLeft == topMiddle && topMiddle == topRight && topRight
+    || middleLeft == middleMiddle && middleMiddle == middleRight && middleRight
+    || bottomLeft == bottomMiddle && bottomMiddle == bottomRight && bottomRight
+    || topLeft == middleLeft && middleLeft == bottomLeft && bottomLeft
+    || topMiddle == middleMiddle && middleMiddle == bottomMiddle && bottomMiddle
+    || topRight == middleRight && middleRight == bottomRight && bottomRight
+    || topLeft == middleMiddle && middleMiddle == bottomRight && bottomRight
+    || topRight == middleMiddle && middleMiddle == bottomLeft && bottomLeft
     ) {
-    if (cross) {
-      winner = "O";
-    } else {
-      winner = "X";
-    }
+    winner = isCross ? "X" : "O";
   }
 
-  if (winner != "") {
+  if (winner) {
     gameOver = true;
-    let element = document.getElementById("id01");
-
+    const element = document.getElementById("id01");
     element.innerHTML = "Game over: " +  winner + " wins";
-    element.style.color = "red"
+    element.style.color = "red";
+
     document.getElementById('replay').style.display = "inline-block";
   }
 }
@@ -66,5 +58,3 @@ function checkWinState() {
 function reset() {
   location.reload();
 }
-
-
